b148fc92d8b17dec4253919223e8854738c6c5e2
Myemportal <50528649+Myemportal@users.noreply.github.com>
2025-08-10 14:47:36 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -e
-if [ ! -f /workspace/.install_done ]; then
-  bash /workspace/install.sh
-fi
+
 cd /workspace/stable-diffusion-webui
-python3 launch.py --listen --port 7860 --api &
-wait -n
+
+# Launch the web UI with listen enabled and on port 7860, also enable API
+python3 launch.py --listen --port 7860 --api
